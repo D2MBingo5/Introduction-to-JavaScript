52b10145a9e5a564d352c64b65395f0676b91cbf
Leu Family <romanianfobjr@gmail.com>
2021-06-08 16:58:18 -0500

    solved task 5 and task 7

--- index.js
+++ index.js
@@ -179,8 +179,8 @@ Using the miles function below do the following:
   3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-    /*add your code here*/
+function miles(kilometers){
+    return kilometers * 0.621371;
   }
 
 
@@ -193,9 +193,10 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-    /*add your code here*/
+function feet(cm){
+    return(cm / 30.48);
   }
+  console.log(feet(30));
  
 
 
@@ -229,8 +230,17 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+  if(score >= 90){
+    return('you got an A');
+  }else if(score >= 80 && score <= 89){
+    return('you got a B');
+  }else if(score >= 70 && score <= 79){
+    return('you got a C');
+  }else if(score >= 60 && score <= 69){
+    return('you got a D');
+  }else
+    return('you got an F');
   }
   
   
